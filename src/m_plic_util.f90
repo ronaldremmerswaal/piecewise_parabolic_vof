fbf566c54faa2acc267c71c111265a93240a276d
Ronald Remmerswaal <ronaldremmerswaal@pm.me>
2022-05-13 19:44:30 +0200

    Utilities for PLIC and PPIC

---
+++ src/m_plic_util.f90
@@ -0,0 +1,118 @@
+module m_plic_util
+  real*8, parameter     :: NORMAL_TOL = 1E-12
+contains
+
+  pure function cmpMoments2d(normal, dx, shift) result(moments)
+    implicit none
+
+    real*8, intent(in)  :: normal(2), dx(2), shift
+    real*8              :: moments(3)
+
+    ! Local variables:
+    real*8              :: normal_(2), dx_(2), max_shift_plane
+
+    normal_ = abs(normal)
+    if (normal_(1) >= normal_(2)) then
+      dx_ = dx
+    else
+      normal_ = [normal_(2), normal_(1)]
+      dx_ = [dx(2), dx(1)]
+    endif
+
+    max_shift_plane = dot_product(abs(normal), dx)/2
+    moments = cmpMoments2d_pc(normal_, dx_, shift + max_shift_plane, max_shift_plane)
+  end function
+
+
+  pure function cmpMoments2d_pc(normal, dx, pc, max_shift_plane) result(moments)
+    implicit none
+
+    real*8, intent(in)  :: normal(2), dx(2), pc, max_shift_plane
+    real*8              :: moments(3)
+
+    ! Local variables:
+    real*8              :: pcMod
+    logical             :: largerThanHalf
+
+    
+
+    largerThanHalf = pc > max_shift_plane
+    if (largerThanHalf) then
+      pcMod = 2*max_shift_plane - pc
+    else
+      pcMod = pc
+    endif
+
+    ! We compute moment w.r.t. cell corner
+    if (pcMod <= 0.0) then
+      moments = 0.0
+    elseif (pcMod<normal(1)*dx(1)) then
+      moments(1) = pcMod**2/(2.0*normal(1)*normal(2))
+      moments(2) = (pcMod**2*(2*pcMod - 3*dx(1)*normal(1)))/(12*normal(1)**2*normal(2))
+      moments(3) = (pcMod**2*(2*pcMod - 3*dx(2)*normal(2)))/(12*normal(1)*normal(2)**2)
+    else ! if (pcMod<=max_shift_plane) then
+      moments(1) = pcMod*dx(1)/normal(2) - normal(1)*dx(1)**2/(2.0*normal(2))
+      moments(2) = -(dx(1)**3*normal(1))/(12*normal(2))
+      moments(3) = (dx(1)*(6*pcMod**2 + 2*(dx(1)*normal(1))**2 - 6*dx(1)*normal(1)*pcMod &
+        - 6*dx(2)*normal(2)*pcMod + 3*dx(1)*dx(2)*normal(1)*normal(2)))/(12*normal(2)**2)
+    endif
+
+    if (largerThanHalf) moments(1) = product(dx) - moments(1)
+  end function
+
+  pure real*8 function cmpShift2d(normal, dx, volume) result(shift)
+
+    implicit none
+
+    real*8, intent(in)    :: normal(2), dx(2), volume
+    
+    ! Local variables:
+    real*8              :: normal_(2), dx_(2), max_shift_plane
+
+    normal_ = abs(normal)
+    if (normal_(1) >= normal_(2)) then
+      dx_ = dx
+    else
+      normal_ = [normal_(2), normal_(1)]
+      dx_ = [dx(2), dx(1)]
+    endif
+
+    max_shift_plane = dot_product(abs(normal), dx)/2
+    shift = cmpShift2d_pc(normal_, dx_, volume, max_shift_plane) - max_shift_plane
+
+  end function
+
+  pure real*8 function cmpShift2d_pc(normal, dx, volume, max_shift_plane) result(pc)
+
+    implicit none
+
+    real*8, intent(in)    :: normal(2), dx(2), volume, max_shift_plane
+
+    ! Local variables:
+    real                  :: max_volume_plane              
+    real                  :: pc_max           
+    real                  :: v1, vMod
+    logical               :: largerThanHalf
+
+    max_volume_plane = product(dx)
+    v1  = normal(1) * dx(1)**2 / (2*normal(2))
+
+    largerThanHalf = volume > max_volume_plane / 2.0
+    if (largerThanHalf) then
+      vMod = max_volume_plane - volume
+    else
+      vMod = volume
+    endif
+
+    if (vMod <= 0.0) then
+      pc = 0.0
+    elseif (vMod < v1) then
+      pc = sqrt(2*normal(1)*normal(2)*vMod)
+    else ! if (vMod <= .5*max_volume_plane) then
+      pc = (vMod + normal(1)*dx(1)**2/(2*normal(2))) * normal(2) / dx(1)
+    endif
+
+    if (largerThanHalf) pc = 2*max_shift_plane - pc
+
+  end function
+end module
---
+++ src/m_ppic_util.f90
@@ -0,0 +1,285 @@
+module m_ppic_util
+  use, intrinsic          :: ieee_arithmetic
+  use, intrinsic          :: iso_fortran_env, only: real64, int64
+
+  implicit none
+
+  integer(kind=2), parameter :: LIQUID_PHASE = 1, GAS_PHASE = 4
+
+  real(real64)            :: d_qnan = transfer(9221120237041090560_int64, 1._real64)
+
+  ! NB All functions here assume the normal to be outward pointing, but the normal is stored (yn(:,)) inward pointing
+contains
+
+  real*8 function cmpMoments2d_parabolic(firstMoment, normal, dx, shift, kappa0, relativeTo) result(volume)
+    use m_r2d_parabolic
+
+    implicit none
+
+    real*8, intent(in)    :: normal(2), dx(2), shift, kappa0
+    real*8, intent(out)   :: firstMoment(2)
+    real*8, intent(in), optional :: relativeTo(2)
+
+    ! Local variables
+    type(r2d_poly_f)      :: liquid
+    real*8                :: moments01(3), relativeTo_(2)
+
+    relativeTo_ = normal * shift
+    if (present(relativeTo)) then
+      relativeTo_ = relativeTo_ + relativeTo
+    endif
+
+    call init_box(liquid, [-dx/2.0, dx/2.0])
+    call intersect_with_parabola(moments01, liquid, normal, kappa0, relativeTo_)
+
+    volume = moments01(1)
+    firstMoment = moments01(2:3)
+  end function
+
+  real function cmpShift2d_parabolic(normal, dx, liqVol, kappa0, relTol, moments01, grad_s) result(shift)
+    use m_plic_util,      only: cmpShift2d
+    use m_r2d_parabolic
+    use m_optimization,   only: brent
+
+    implicit none
+
+    real*8, intent(in)    :: normal(2), dx(2), liqVol, kappa0
+    real*8, optional, intent(in) :: relTol
+    real*8, optional, intent(out) :: moments01(3), grad_s(2)
+
+    ! Local variables
+    type(r2d_poly_f)      :: cell
+    real*8                :: moments01_(3), max_shift_plane, cellVol, shift_plane, plane_err
+    real*8                :: shift_l, shift_r, err_l, err_r, normal3(3), dx3(3)
+    real*8                :: relTol_, grad_s_(2)
+
+    max_shift_plane = dot_product(abs(normal), dx)/2
+
+    cellVol = product(dx)
+    if (liqVol <= 0.0) then
+      shift = ieee_value(1.0_real64,  ieee_negative_inf)
+      if (present(moments01)) moments01 = 0.0
+    elseif (liqVol >= cellVol) then
+      shift = ieee_value(1.0_real64,  ieee_positive_inf)
+      if (present(moments01)) moments01 = 0.0
+    else
+      call init_box(cell, [-dx/2.0, dx/2.0])
+
+      ! Use PLIC to get a good initial bracket (one side at least)
+      shift_plane = cmpShift2d(normal, dx, liqVol)
+      plane_err = volume_error_function(shift_plane)
+
+      ! Try to get a better bracket with an (educated) guess
+      if (plane_err == 0.0) then
+        ! iff kappa0 == 0.0
+        if (present(moments01)) moments01 = moments01_
+        if (present(grad_s)) grad_s = grad_s_
+        return
+      elseif (plane_err > 0.0) then
+        ! iff kappa0 < 0.0
+        shift_r = shift_plane
+        err_r = plane_err
+
+        shift_l = max_shift_plane * (kappa0 * max_shift_plane / 2. - 1.)
+        err_l = -liqVol
+      else
+        ! iff kappa0 > 0.0
+        shift_l = shift_plane
+        err_l = plane_err
+
+        shift_r = max_shift_plane * (kappa0 * max_shift_plane / 2. + 1.)
+        err_r = cellVol - liqVol
+      endif
+
+      if (present(relTol)) then
+        relTol_ = relTol
+      else
+        relTol_ = 1E-15
+      endif
+
+      ! NB the centered limits -pc_shift, pc_shift are incorrect for a parabolic interface
+      shift = brent(volume_error_function, shift_l, shift_r, max_shift_plane * relTol_, 30, err_l, err_r)
+    endif
+
+    if (present(moments01)) moments01 = moments01_
+    if (present(grad_s)) grad_s = grad_s_
+  contains
+
+    real*8 function volume_error_function(pc_tmp) result(err)
+      implicit none
+
+      real*8, intent(in)    :: pc_tmp
+
+      ! Local variables
+      type(r2d_poly_f)    :: liquid
+      integer             :: idx
+
+      grad_s_ = [d_qnan, d_qnan]
+      call copy(to=liquid, from=cell)
+      call intersect_with_parabola(moments01_, liquid, normal, kappa0, normal * pc_tmp, grad_s=grad_s_)
+      err = moments01_(1) - liqVol
+    end function
+  end function
+
+
+  real function cmpSymmetricDifference2d_parabolic(x, dx, normal, shift, kappa0, customFun) result(sd)
+    use m_r2d_parabolic
+    implicit none
+
+    real*8, intent(in)     :: x(2), dx(2), normal(2), shift, kappa0
+    real*8, external, optional :: customFun
+
+    ! Local variables
+    real*8                :: sd_1(3), sd_2(3), normal_(2)
+    type(r2d_poly_f)      :: exact_gas, exact_liq
+
+    normal_ = -normal
+
+    ! Construct polygonal approximation of exact gas & liquid domains
+    ! (relative to the cell centroid)
+    if (present(customFun)) then
+      call get_polygonal_approximation_of_exact_domain(exact_gas, GAS_PHASE, x, dx, customFun)
+      call get_polygonal_approximation_of_exact_domain(exact_liq, LIQUID_PHASE, x, dx, customFun)
+    else
+      call get_polygonal_approximation_of_exact_domain(exact_gas, GAS_PHASE, x, dx)
+      call get_polygonal_approximation_of_exact_domain(exact_liq, LIQUID_PHASE, x, dx)
+    endif
+
+    ! Compute symmetric difference
+    call intersect_with_parabola(sd_1, exact_gas, normal_, kappa0, x + normal_ * shift)
+    call intersect_with_parabola(sd_2, exact_liq, -normal_, -kappa0, x + normal_ * shift)
+    sd = sd_1(1) + sd_2(1)
+  end
+
+  subroutine get_polygonal_approximation_of_exact_domain(poly, phase, xc, dx, customFun)
+    use m_optimization,   only: brent
+    use m_r2d_parabolic
+    implicit none
+
+    type(r2d_poly_f), intent(out) :: poly
+    integer*2, intent(in) :: phase
+    real*8, intent(in)    :: xc(2), dx(2)
+    real*8, external, optional :: customFun
+
+    ! Local variables
+    real*8                :: pos(2, R2D_MAX_VERTS), pos_skeleton(2, 8), corners(2, 4), funVals(4)
+    real*8                :: x0(2), dir(2), step, tDir(2)
+    integer               :: edx, vdx, vdx_first_inside, nrPos, vdx_next, nrPos_skelelton, rdx
+    integer, parameter    :: VERTS_PER_SEGMENT = R2D_MAX_VERTS / 3
+    logical               :: vdx_is_inside, vdx_next_is_inside, is_on_parabola(8)
+
+    corners(:,1) = xc + [-dx(1), -dx(2)]/2
+    corners(:,2) = xc + [dx(1), -dx(2)]/2
+    corners(:,3) = xc + [dx(1), dx(2)]/2
+    corners(:,4) = xc + [-dx(1), dx(2)]/2
+
+    ! Find out which corners of the cell are inside the domain
+    vdx_first_inside = 0
+    do vdx=1,4
+      funVals(vdx) = interfaceFun(corners(:,vdx))
+      if (funVals(vdx) >= 0.0 .and. vdx_first_inside == 0) vdx_first_inside = vdx
+    enddo
+    if (vdx_first_inside == 0) then
+      poly%nverts = 0
+      return
+    endif
+
+    ! Loop over the edges and construct the polygonal 'skeleton'
+    vdx = vdx_first_inside
+    vdx_is_inside = .true.
+    nrPos_skelelton = 0
+
+    do edx=1,4
+      vdx_next = merge(1, vdx + 1, vdx == 4)
+      vdx_next_is_inside = funVals(vdx_next) >= 0.0
+
+      ! TODO so far we assume that an edge has at most one intersection
+      if (vdx_is_inside .and. vdx_next_is_inside) then
+        ! Add next node (corner)
+        nrPos_skelelton = nrPos_skelelton + 1
+        pos_skeleton(:,nrPos_skelelton) = corners(:,vdx_next)
+        is_on_parabola(nrPos_skelelton) = .false.
+      elseif (vdx_is_inside .and. .not. vdx_next_is_inside) then
+        ! Find and add new position
+        x0 = corners(:,vdx)
+        dir = corners(:,vdx_next) - corners(:,vdx)
+        step = brent(interfaceFun_step, 0.0D0, 1.0D0, 1D-15, 30, funVals(vdx), funVals(vdx_next))
+        nrPos_skelelton = nrPos_skelelton + 1
+        pos_skeleton(:,nrPos_skelelton) = x0 + step * dir
+        is_on_parabola(nrPos_skelelton) = .true.
+      elseif (.not. vdx_is_inside .and. vdx_next_is_inside) then
+        ! Find and add new position
+        x0 = corners(:,vdx)
+        dir = corners(:,vdx_next) - corners(:,vdx)
+        step = brent(interfaceFun_step, 0.0D0, 1.0D0, 1D-15, 30, funVals(vdx), funVals(vdx_next))
+        nrPos_skelelton = nrPos_skelelton + 1
+        pos_skeleton(:,nrPos_skelelton) = x0 + step * dir
+        is_on_parabola(nrPos_skelelton) = .true.
+
+        ! And add next node (corner)
+        nrPos_skelelton = nrPos_skelelton + 1
+        pos_skeleton(:,nrPos_skelelton) = corners(:,vdx_next)
+        is_on_parabola(nrPos_skelelton) = .false.
+      endif
+
+      vdx = vdx_next
+      vdx_is_inside = vdx_next_is_inside
+    enddo
+
+    ! Now we add a refined approximation on edges that are on the interface
+    nrPos = 0
+    vdx = 1
+    do edx=1,nrPos_skelelton
+      vdx_next = merge(1, vdx + 1, vdx == nrPos_skelelton)
+
+      ! Add (refinement of) the half open interval (pos_skeleton(:,vdx),pos_skeleton(:,vdx_next)]
+      if (is_on_parabola(vdx) .and. is_on_parabola(vdx_next)) then
+
+        tDir = pos_skeleton(:,vdx_next) - pos_skeleton(:,vdx)
+        if (norm2(tDir) < 1E-15 * dx(1)) then
+          nrPos = nrPos + 1
+          pos(:,nrPos) = pos_skeleton(:,vdx_next)
+        else
+          ! Refine the face
+
+          ! Make dir normal to the face
+          dir = [-tDir(2), tDir(1)]
+          do rdx=1,VERTS_PER_SEGMENT
+            x0 = pos_skeleton(:,vdx) + rdx * tDir / VERTS_PER_SEGMENT
+
+            ! We impose here that the radius of curvature of the interface is bounded from below by half (relative to the mesh spacing)
+            step = brent(interfaceFun_step, -.5D0, .5D0, 1D-15, 30)
+
+            nrPos = nrPos + 1
+            pos(:,nrPos) = x0 + step * dir
+          enddo
+        endif
+      else
+        nrPos = nrPos + 1
+        pos(:,nrPos) = pos_skeleton(:,vdx_next)
+      endif
+
+      vdx = vdx_next
+    enddo
+    call init_from_pos(poly, pos(:,1:nrPos))
+  contains
+
+    real*8 function interfaceFun(x) result(f)
+  
+      implicit none
+
+      real*8, intent(in)  :: x(2)
+
+      f = customFun(x)
+      if (phase == GAS_PHASE) f = -f
+    end
+
+    real*8 function interfaceFun_step(step_) result(f)
+      implicit none
+
+      real*8, intent(in)    :: step_
+
+      f = interfaceFun(x0 + step_ * dir)
+    end
+  end
+end module
